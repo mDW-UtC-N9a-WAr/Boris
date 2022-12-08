a3628706b604ab605a8b93739f0a9d2295b486b7
jak <jak_130@mail.ru>
2022-12-08 12:10:51 +0300

    Матрица

--- Examples004_int_matrix/Program.cs
+++ Examples004_int_matrix/Program.cs
@@ -1,9 +1,26 @@
-﻿int[,] matrix = new int[3,4];
-
-for (int i = 0; i < 3; i++)
+﻿void PrintArray(int[,] matr)
 {
-    for (int j = 0; j < 4; j++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        Console.WriteLine($" {matrix[i, j]} ");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($" {matr[i, j]} ");
+        }
+    Console.WriteLine();
     }
 }
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);//(1;10)
+        }
+    }
+}
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
