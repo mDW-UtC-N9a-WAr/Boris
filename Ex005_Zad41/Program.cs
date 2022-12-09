19a277d69d1134f606a732109310cdd963e33e81
jak <jak_130@mail.ru>
2022-12-09 23:04:53 +0300

    Задача1

---
+++ Ex005_Zad41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел
+// больше 0 ввёл пользователь.
+
+Console.Write("Введите числа: ");
+int[] numbers = intParse(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
